05096c29a9d07cecc604c6930657e6b2d97dcc49
R. Pablo <rpablo.gdr@gmail.com>
2021-06-01 20:37:01 -0300

    Adding gitignore file

---
+++ src/components/UsersList.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import Header from "./Header";
+import Footer from "./Footer";
+
+function UsersRegister() {
+  return (
+    <>
+      <Header />
+        
+      <Footer />
+    </>
+  );
+}
+
+export default UsersRegister;
